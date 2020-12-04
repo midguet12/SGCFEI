39561c1fae9725b6e5247ebbbb31051f81f99429
Seth Díaz <33437473+Sethndd@users.noreply.github.com>
2020-12-04 15:02:20 -0600

    Proyecto añadido

---
+++ SGCFEI/src/sgcfei/menus/Coordinador/MenuController.java
@@ -0,0 +1,47 @@
+package sgcfei.menus.Coordinador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+public class MenuController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void registrarPlanAcademia(MouseEvent event) {
+    }
+
+    @FXML
+    private void consultarPlanAcademia(MouseEvent event) {
+    }
+
+    @FXML
+    private void registrarPlanCurso(MouseEvent event) {
+    }
+
+    @FXML
+    private void consultarPlanCurso(MouseEvent event) {
+    }
+
+    @FXML
+    private void registrarMinuta(MouseEvent event) {
+    }
+
+    @FXML
+    private void consultarMinuta(MouseEvent event) {
+    }
+
+    @FXML
+    private void registrarPlanEstudios(MouseEvent event) {
+    }
+
+    @FXML
+    private void consultarPlanEstudios(MouseEvent event) {
+    }
+}
