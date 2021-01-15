54596924b27c326ec5d4c054750e2a0120148952
Seth Díaz <33437473+Sethndd@users.noreply.github.com>
2021-01-15 01:59:21 -0600

    Configuración de base de datos
    Clase para crear y leer el archivo de configuración de base de datos.

---
+++ SGCFEI/src/sgcfei/HerramientaDB.java
@@ -0,0 +1,19 @@
+package main;
+
+import database.ControladorCredenciales;
+import database.DBCredenciales;
+
+public class HerramientaDB {
+    public static void main(String[] args) {
+        //Crear nuevo archivo db.conf
+        DBCredenciales cred = new DBCredenciales("maisonbleue2020.ddns.net", "3306", "pruebas", "seth", "Sndd1026");
+        ControladorCredenciales.escribir(cred);
+        
+        //Leer datos de db.conf
+        DBCredenciales cred2 = ControladorCredenciales.leer();
+        
+        System.out.println(cred2);
+
+    }
+}
+
