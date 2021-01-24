518408746d6fd40389edf79072100822a23c3a77
midguet12 <midguet12@hotmail.com>
2021-01-24 14:50:48 -0500

    Update ExperienciaEducativa.java

--- SGCFEI/src/pojos/ExperienciaEducativa.java
+++ SGCFEI/src/pojos/ExperienciaEducativa.java
@@ -34,21 +34,22 @@ public class ExperienciaEducativa implements POJO{
 	academia int not null,
 	primary key (ncr)
     */
-    
 
-    public ExperienciaEducativa(int nrc, String nombre, int idAcademia) {
+    public ExperienciaEducativa(int nrc, String codigo, String nombre, String areaFormacionPrincipal, String areaFormacionSecundaria, int creditos, int teoria, int totalHoras, String equivalencias, String modalidad, String oportunidadesEvaluacion, String requisitos, String coRequisitos, int idAcademia) {
         this.nrc = nrc;
+        this.codigo = codigo;
         this.nombre = nombre;
+        this.areaFormacionPrincipal = areaFormacionPrincipal;
+        this.areaFormacionSecundaria = areaFormacionSecundaria;
+        this.creditos = creditos;
+        this.teoria = teoria;
+        this.totalHoras = totalHoras;
+        this.equivalencias = equivalencias;
+        this.modalidad = modalidad;
+        this.oportunidadesEvaluacion = oportunidadesEvaluacion;
+        this.requisitos = requisitos;
+        this.coRequisitos = coRequisitos;
         this.idAcademia = idAcademia;
-    }
-
-    public ExperienciaEducativa(String nombre, int idAcademia) {
-        this.nombre = nombre;
-        this.idAcademia = idAcademia;
-    }
-
-    public ExperienciaEducativa() {
-        
     }
 
     public int getNrc() {
@@ -59,12 +60,100 @@ public class ExperienciaEducativa implements POJO{
         this.nrc = nrc;
     }
 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
     public String getNombre() {
         return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getAreaFormacionPrincipal() {
+        return areaFormacionPrincipal;
+    }
+
+    public void setAreaFormacionPrincipal(String areaFormacionPrincipal) {
+        this.areaFormacionPrincipal = areaFormacionPrincipal;
+    }
+
+    public String getAreaFormacionSecundaria() {
+        return areaFormacionSecundaria;
+    }
+
+    public void setAreaFormacionSecundaria(String areaFormacionSecundaria) {
+        this.areaFormacionSecundaria = areaFormacionSecundaria;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public int getTeoria() {
+        return teoria;
+    }
+
+    public void setTeoria(int teoria) {
+        this.teoria = teoria;
+    }
+
+    public int getTotalHoras() {
+        return totalHoras;
+    }
+
+    public void setTotalHoras(int totalHoras) {
+        this.totalHoras = totalHoras;
+    }
+
+    public String getEquivalencias() {
+        return equivalencias;
+    }
+
+    public void setEquivalencias(String equivalencias) {
+        this.equivalencias = equivalencias;
+    }
+
+    public String getModalidad() {
+        return modalidad;
+    }
+
+    public void setModalidad(String modalidad) {
+        this.modalidad = modalidad;
+    }
+
+    public String getOportunidadesEvaluacion() {
+        return oportunidadesEvaluacion;
+    }
+
+    public void setOportunidadesEvaluacion(String oportunidadesEvaluacion) {
+        this.oportunidadesEvaluacion = oportunidadesEvaluacion;
+    }
+
+    public String getRequisitos() {
+        return requisitos;
+    }
+
+    public void setRequisitos(String requisitos) {
+        this.requisitos = requisitos;
+    }
+
+    public String getCoRequisitos() {
+        return coRequisitos;
+    }
+
+    public void setCoRequisitos(String coRequisitos) {
+        this.coRequisitos = coRequisitos;
     }
 
     public int getIdAcademia() {
@@ -74,7 +163,10 @@ public class ExperienciaEducativa implements POJO{
     public void setIdAcademia(int idAcademia) {
         this.idAcademia = idAcademia;
     }
+    
+    
 
+    
     @Override
     public String toString() {
         return "ExperienciaEducativa{" + "nrc=" + nrc + ", nombre=" + nombre + ", idAcademia=" + idAcademia + '}';
