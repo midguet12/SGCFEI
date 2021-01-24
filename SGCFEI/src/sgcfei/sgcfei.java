9aff6ae7f25aaadb0c6ad7674dc59b41a481b1a6
midguet12 <midguet12@hotmail.com>
2021-01-24 01:53:39 -0500

    add

--- SGCFEI/src/sgcfei/sgcfei.java
+++ SGCFEI/src/sgcfei/sgcfei.java
@@ -1,10 +1,12 @@
 package sgcfei;
 
+import accesodatos.UsuarioDAO;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pojos.Usuario;
 
 public class sgcfei extends Application{
 
@@ -14,10 +16,21 @@ public class sgcfei extends Application{
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/sgcfei/menus/Login.fxml"));
+        /*Parent root = FXMLLoader.load(getClass().getResource("/sgcfei/menus/Login.fxml"));
         stage.setTitle("Login");
         stage.setScene(new Scene(root, 300, 300));
-        stage.show();
+        stage.show();*/
+        
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        
+        Usuario usuario = usuarioDAO.obtener(1);
+        
+        String x = usuario.getUsername();
+        
+        System.out.println(x);
+        
+        
+        
     }
     
 }
