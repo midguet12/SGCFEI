099546917b87ab88a9464e0c8111d95cdc212d11
Seth Díaz <33437473+Sethndd@users.noreply.github.com>
2021-01-23 20:06:14 -0600

    Catálogo de Academias
    Catálogo de academias completado

---
+++ SGCFEI/src/sgcfei/menus/Administrador/Catalogos/Academia/AgregarController.java
@@ -0,0 +1,103 @@
+package sgcfei.menus.Administrador.Catalogos.Academia;
+
+import accesodatos.AcademiaDAO;
+import accesodatos.AcademicoDAO;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import pojos.Academia;
+import pojos.Academico;
+import util.ControladorVentanas;
+import util.Validador;
+
+public class AgregarController implements Initializable {
+    @FXML
+    private Button btnAceptar = new Button();
+    @FXML
+    private Button btnCancelar = new Button();
+    @FXML
+    private TextField tfNombre;
+    @FXML
+    private TextField tfDescripcion;
+    @FXML
+    private ComboBox<Academico> cboxCoordinador;
+    private ObservableList<Academico> coordinadores = FXCollections.observableArrayList();
+    private boolean datosCorrectos = true;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        AcademicoDAO dao =  new AcademicoDAO();
+        coordinadores.setAll(dao.obtenerAcademicosPorRol("Coordinador"));
+        cboxCoordinador.setItems(coordinadores);
+        cboxCoordinador.getSelectionModel().select(0);
+    }    
+
+    @FXML
+    private void aceptar(MouseEvent event) {
+        String nombre = tfNombre.getText();
+        String descripcion = tfDescripcion.getText();
+        String idCoordinador = cboxCoordinador.getValue().getNumeroPersonal();
+        
+        if(!Validador.validarNombre(nombre)){
+            datosCorrectos = false;
+        }
+        else if(nombre.length() >= 255){
+            datosCorrectos = false;
+        }
+        else if(descripcion.length() >= 255){
+            datosCorrectos = false;
+        }
+        
+        if(nombre.isEmpty() || descripcion.isEmpty()){
+            Alert alerta = ControladorVentanas.crearAlerta("Campos vacios",
+                    "No se han llenado todos los campos, por favor verificar", Alert.AlertType.ERROR);
+            alerta.showAndWait();
+        }
+        else{
+            if(datosCorrectos){
+                Academia academia = new Academia(nombre, descripcion, idCoordinador);            
+                AcademiaDAO dao = new AcademiaDAO();
+                dao.insertar(academia);
+                
+                Alert alerta = ControladorVentanas.crearAlerta("Operación exitosa",
+                        "Se ha agregado una academia correctamente", Alert.AlertType.INFORMATION);
+                alerta.showAndWait();
+                
+                cerrar();
+            }
+            else{
+                Alert alerta = ControladorVentanas.crearAlerta("Datos incorrectos",
+                        "Los datos ingresados no son del tipo adecuado, por favor verificar", Alert.AlertType.ERROR);
+                alerta.showAndWait();
+            }
+        }
+    }
+
+    @FXML
+    private void cancelar(MouseEvent event) {
+        Alert alerta = ControladorVentanas.crearAlerta("Confirmación",
+                        "¿Está seguro que desea cancelar el registro?", Alert.AlertType.CONFIRMATION);
+        Optional<ButtonType> respuesta = alerta.showAndWait();
+        
+        if(respuesta.get() == ButtonType.OK){
+            cerrar();
+        }
+    }
+    
+    private void cerrar(){
+        Stage stageActual = (Stage) btnCancelar.getScene().getWindow();
+        ControladorVentanas.abrirYCerrar("/sgcfei/menus/Administrador/Catalogos/Academia/Registros.fxml",
+                    "Catálogo de Academia", stageActual);
+    }
+}
---
+++ SGCFEI/src/sgcfei/menus/Administrador/Catalogos/Academia/RegistrosController.java
@@ -0,0 +1,129 @@
+package sgcfei.menus.Administrador.Catalogos.Academia;
+
+import accesodatos.AcademiaDAO;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import pojos.Academia;
+import util.ControladorVentanas;
+
+public class RegistrosController implements Initializable {
+    @FXML
+    private TableView<Academia> tabla;
+    @FXML
+    private TableColumn<Academia, String> cNombre;
+    @FXML
+    private Button btnAgregar = new Button();
+    @FXML
+    private Button btnConsultar = new Button();
+    @FXML
+    private Button btnEditar = new Button();
+    @FXML
+    private Button btnEliminar = new Button();
+    @FXML
+    private Button btnCancelar = new Button();
+    private ObservableList<Academia> listaAcademias;
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        cargarTabla();
+    } 
+    
+    @FXML
+    private void clickTabla(MouseEvent event) {
+        if(event.getClickCount() == 2){
+            consultar();
+        }
+    }
+    
+    @FXML
+    private void clickAgregar(MouseEvent event) {
+        Stage stageActual = (Stage) btnAgregar.getScene().getWindow();
+        ControladorVentanas.abrirYCerrar("/sgcfei/menus/Administrador/Catalogos/Academia/Agregar.fxml",
+                    "Agregar Academia", stageActual);
+    }
+    
+    @FXML
+    private void clickConsultar(MouseEvent event) {
+        consultar();
+    }
+
+    @FXML
+    private void clickEditar(MouseEvent event) {
+        if(validarSeleccion()){
+            Academia academia = tabla.getSelectionModel().getSelectedItem();
+            Stage stageActual = (Stage) btnAgregar.getScene().getWindow();
+            EditarController controlador = new EditarController(academia);
+            ControladorVentanas.abrirYCerrarConControlador("/sgcfei/menus/Administrador/Catalogos/Academia/Editar.fxml",
+                    "Editar Academia", controlador, stageActual);
+        }
+    }
+
+    @FXML
+    private void clickEliminar(MouseEvent event) {
+        Alert alerta = ControladorVentanas.crearAlerta("Confirmación",
+                        "¿Está seguro que desea eliminar esta academia?", Alert.AlertType.CONFIRMATION);
+        Optional<ButtonType> respuesta = alerta.showAndWait();
+        
+        if(respuesta.get() == ButtonType.OK){
+            int idAcademia = tabla.getSelectionModel().getSelectedItem().getIdAcademia();
+            
+            AcademiaDAO dao = new AcademiaDAO();
+            dao.eliminar(idAcademia);
+            
+            alerta = ControladorVentanas.crearAlerta("Operación exitosa",
+                        "Se ha eliminado la academia correctamente", Alert.AlertType.INFORMATION);
+            alerta.showAndWait();
+            cargarTabla();
+        }
+    }
+
+    @FXML
+    private void clickCancelar(MouseEvent event) {
+        Stage stageActual = (Stage) btnCancelar.getScene().getWindow();
+        ControladorVentanas.abrirYCerrar("/sgcfei/menus/Administrador/Catalogos/SeleccionarCatalogo.fxml",
+                    "Consultar Academia", stageActual);
+    }
+    
+    private void cargarTabla(){
+        AcademiaDAO dao = new AcademiaDAO();
+        listaAcademias = FXCollections.observableArrayList();
+        
+        cNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        listaAcademias.addAll(dao.obtenerTodasAcademias());
+        tabla.setItems(listaAcademias);
+    }
+    
+    private void consultar(){
+        if(validarSeleccion()){
+            Academia academia = tabla.getSelectionModel().getSelectedItem();
+            Stage stageActual = (Stage) btnAgregar.getScene().getWindow();
+            ConsultarController controlador = new ConsultarController(academia);
+            ControladorVentanas.abrirYCerrarConControlador("/sgcfei/menus/Administrador/Catalogos/Academia/Consultar.fxml",
+                    "Consultar Academia", controlador, stageActual);
+        }
+    }
+    
+    private boolean validarSeleccion(){
+        if(tabla.getSelectionModel().isEmpty()){
+            Alert alerta = ControladorVentanas.crearAlerta("Elemento no seleccionado",
+                       "Debe seleccionar un elemento de la tabla", Alert.AlertType.ERROR);
+            alerta.showAndWait();
+            return false;
+        }
+        return true;
+    }
+}
