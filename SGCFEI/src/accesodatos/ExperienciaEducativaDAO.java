3e23cbc77880608c26189146a456fd8d2c9edf59
midguet12 <midguet12@hotmail.com>
2021-01-25 18:34:06 -0500

    add

--- SGCFEI/src/accesodatos/ExperienciaEducativaDAO.java
+++ SGCFEI/src/accesodatos/ExperienciaEducativaDAO.java
@@ -214,4 +214,34 @@ public class ExperienciaEducativaDAO implements DAO{
 
             return experiencias;
         }
+    
+        public List<String> obtenerNombres(){
+            List<String> nombres = new ArrayList<>();
+            conexion = db.obtenerConexion();
+            String consulta = "Select nombre from experienciaEducativa;";
+        
+            try{
+                PreparedStatement consultaPreparada = conexion.prepareStatement(consulta);
+                resultados = consultaPreparada.executeQuery();
+                while (resultados.next()){
+                    nombres.add(resultados.getString("nombre"));
+                }
+
+            }
+            catch (SQLException ex){    
+                RegistroExcepciones.escribirExcepcion(ex, this.getClass().getName());
+            } 
+            catch (NullPointerException ex){
+                RegistroExcepciones.escribirExcepcion(ex, this.getClass().getName());
+            }
+            catch (Exception ex){
+                RegistroExcepciones.escribirExcepcion(ex, this.getClass().getName());
+            }
+            finally{
+                db.cerrarConexion();
+            }
+        
+        
+            return nombres;
+        }
 }
